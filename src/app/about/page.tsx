d0b81b1ca929eaec9fb8a40ecb67d82c820cddf6
Gakuto Tobinaga <g-tobinaga@fabrica-com.co.jp>
2025-01-17 06:14:10 +0900

    fix: insert sample about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+
+export default function About() {
+  return (
+    <div className="min-h-screen bg-background">
+        This page is under construction.
+    </div>
+  )
+}
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@ import { Navbar } from '@/components/navbar'
 import { Hero } from '@/components/hero'
 import { Products } from '@/components/products'
 import { Partners } from '@/components/partners'
-import { Contact } from '@/components/contact'
 import { Footer } from '@/components/footer'
-import Image from 'next/image'
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-background">
